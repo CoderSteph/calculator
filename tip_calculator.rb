2d8a07cfd6e51ef9a1c429d00418df804fb2c0a3
codersteph <tena_stephanie@yahoo.com>
2016-07-28 18:33:52 +0000

    Done

--- tip_calculator.rb
+++ tip_calculator.rb
@@ -25,12 +25,28 @@ def calculate_tip(total,rating)
       total_bill_four = total + tip_four
       "Your total tip is #{tip_four}. You tipped your waiter 20%. Your total bill is #{total_bill_four}. Thanks for your service!"
 
-   else rating == 5
+   elsif rating == 5
       tip_five = total * 0.25
       total_bill_five = total + tip_five
       "Your total tip is #{tip_five}. You tipped your wiater 25%. Your total bill is #{total_bill_five}. Thanks for your service!"
+      else
+      puts "Sorry. You need to put a number from 1-5."
    end
 end
 
 puts calculate_tip(user_total,user_rating)
+
+puts "Have you been here before? "
+user_answer = gets.chomp
+if user_answer == "yes"
+   puts "What did you tip for your service last visit?"
+   user_last_tip = gets.chomp.to_f
+   puts "What did you tip this visit?"
+   user_answer = gets.chomp.to_f
+   total_tips = user_last_tip + user_answer
+   puts "Your total tips for all the times you have visited are #{total_tips}. You will get a 10% discount on your next visit."
+   else
+end
+
+
 
